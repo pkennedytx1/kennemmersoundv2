c1b1932a5d58c8be2d538b6197d088c39e2ac592
Patrick Kennedy <pkennedytx1@gmail.com>
2021-03-05 16:23:24 -0600

    fixed scroll to top porblem and added contact button always visable

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router} from 'react-router-dom';
 import "animate.css/animate.min.css";
 import { ToastContainer } from 'react-toastify';
 import { firebaseConfig } from './firebaseConfig';
+import { Link } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop'
 import firebase from 'firebase';
 import Footer from './components/Footer';
 import './app.css'
@@ -15,11 +17,18 @@ function App() {
   return (
     <div className='site'>
       <Router>
+        <ScrollToTop>
         <div className='site-content'>
           <ToastContainer />
           <Routes />
         </div>
         <Footer />
+        <Link to='/contact'>
+          <div className="site-button">
+            <h3><i class="bi bi-chat"></i></h3>
+          </div>
+        </Link>
+        </ScrollToTop>
       </Router>
     </div>
   );
